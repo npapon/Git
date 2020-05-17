89bf25cbb58ed6824b8ecaeb1eb2e3e5edca996b
Papoune <npapon@live.fr>
2020-05-17 11:29:27 +0200

    un nouveau depart

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,7 @@ public class Main {
     public static void main( String[] args ) {
         // TODO Auto-generated method stub
 
-<<<<<<< HEAD
-        System.out.println( "residentevil" );
-=======
-        System.out.println( "ff" );
->>>>>>> ba9b022f604d8d9f97efb526e94cb9ecef247244
+        System.out.println( "master" );
     }
 
 }
