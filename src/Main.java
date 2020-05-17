58b97bcdb2c161e1f3c0e83979bec6d82f89ccf1
Papoune <npapon@live.fr>
2020-05-17 08:55:30 +0200

    d

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,11 @@ public class Main {
     public static void main( String[] args ) {
         // TODO Auto-generated method stub
 
+<<<<<<< HEAD
         System.out.println( "c" );
+=======
+        System.out.println( "a" );
+>>>>>>> parent of 2dc89a2... b
     }
 
 }
